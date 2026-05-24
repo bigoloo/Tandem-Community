22ea89d9ced38319a2234d5829f5aa8d67e8f9e8
Amin Bahiraee <mr.bahiraee@gmail.com>
2026-05-24 13:12:41 +0200

    chore(deps): remove ui-text-google-fonts dependency and font_certs.xml
    No longer needed since fonts are bundled as local assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.font)
 
     implementation(platform(libs.koin.bom))
     implementation(libs.koin.android)
